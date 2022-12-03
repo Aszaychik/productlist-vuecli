78ade0bcb0e87674a69d61d553e47b8584968d1d
AsZaychik <aszaychik@gmail.com>
2022-12-03 11:41:29 +0700

    Price Component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,22 @@
   <main id="app" class="container mt-5">
     <h1>ASZShop</h1>
     <p class="animate__animated animate__fadeInRight">Lorem ipsum dolor sit</p>
-    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <div class="d-flex justify-content-center align-items-center gap-3">
+      <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+      <Price :value="69"></Price>
+    </div>
   </main>
 </template>
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import Price from "./components/Price.vue";
 
 export default {
   name: "App",
   components: {
     FontAwesomeIcon,
+    Price,
   },
 };
 </script>
---
+++ src/components/Price.vue
@@ -0,0 +1,21 @@
+<template>
+  <span>
+    {{ this.currency + Number.parseFloat(this.value).toFixed(this.precision) }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Number,
+    currency: {
+      type: String,
+      default: "$",
+    },
+    precision: {
+      type: Number,
+      default: 2,
+    },
+  },
+};
+</script>
